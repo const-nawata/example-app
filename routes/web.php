57c9ab18a6a724d9560ffeed490bd5d1ff5abe20
Kostiantyn <const.nawata@gmail.com>
2022-08-23 15:45:06 +0300

    Show links

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,7 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $links = \App\Models\Link::all();
+
+    return view('welcome', ['links' => $links]);
 });
 
 Auth::routes();
